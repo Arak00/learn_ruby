4014e001b499b57ed88cf064c79b3cd60cfe7159
Alex <Alexandrecartier00@gmail.com>
2018-04-10 00:31:34 +0200

    dernier push

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -15,6 +15,12 @@ def repeat(hello, n=2)
   hello + (" #{hello}" * (n-1))
   end
 
-def start_of_word(hello, 1)
-  return hello[0].to_a
+def start_of_word(word, n)
+      [word]
+      return word[0..n-1]
+end
+
+def first_word(word)
+     word = word.split()
+     word[0]
 end
